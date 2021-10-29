cd2992e85d429a72e1615fb9fdd6b78eb724a5cf
WorldSilver <85555472+WorldSilver@users.noreply.github.com>
2021-10-29 17:32:30 +0200

    modifiche al menu principale + rispristino al vecchio input sistem + creato nuova scena

--- Team Charizard Game/Assets/Rob/Code/CursoreControll.cs
+++ Team Charizard Game/Assets/Rob/Code/CursoreControll.cs
@@ -1,132 +1,132 @@
-// GENERATED AUTOMATICALLY FROM 'Assets/Rob/png/CursoreControll.inputactions'
+//// GENERATED AUTOMATICALLY FROM 'Assets/Rob/png/CursoreControll.inputactions'
 
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine.InputSystem;
-using UnityEngine.InputSystem.Utilities;
+//using System;
+//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine.InputSystem;
+//using UnityEngine.InputSystem.Utilities;
 
-public class @CursoreControll : IInputActionCollection, IDisposable
-{
-    public InputActionAsset asset { get; }
-    public @CursoreControll()
-    {
-        asset = InputActionAsset.FromJson(@"{
-    ""name"": ""CursoreControll"",
-    ""maps"": [
-        {
-            ""name"": ""Mouse"",
-            ""id"": ""1f7fd6fd-cc36-4fbd-857e-601d244dff8a"",
-            ""actions"": [
-                {
-                    ""name"": ""Click"",
-                    ""type"": ""Button"",
-                    ""id"": ""d74422f0-ab3a-49ef-a6c7-789ea2f3bd2b"",
-                    ""expectedControlType"": ""Button"",
-                    ""processors"": """",
-                    ""interactions"": """"
-                }
-            ],
-            ""bindings"": [
-                {
-                    ""name"": """",
-                    ""id"": ""2ec476de-498d-49ed-bfdf-3aa195c2c3b4"",
-                    ""path"": ""<Mouse>/leftButton"",
-                    ""interactions"": ""Press(behavior=1)"",
-                    ""processors"": """",
-                    ""groups"": """",
-                    ""action"": ""Click"",
-                    ""isComposite"": false,
-                    ""isPartOfComposite"": false
-                }
-            ]
-        }
-    ],
-    ""controlSchemes"": []
-}");
-        // Mouse
-        m_Mouse = asset.FindActionMap("Mouse", throwIfNotFound: true);
-        m_Mouse_Click = m_Mouse.FindAction("Click", throwIfNotFound: true);
-    }
+//public class @CursoreControll : IInputActionCollection, IDisposable
+//{
+//    public InputActionAsset asset { get; }
+//    public @CursoreControll()
+//    {
+//        asset = InputActionAsset.FromJson(@"{
+//    ""name"": ""CursoreControll"",
+//    ""maps"": [
+//        {
+//            ""name"": ""Mouse"",
+//            ""id"": ""1f7fd6fd-cc36-4fbd-857e-601d244dff8a"",
+//            ""actions"": [
+//                {
+//                    ""name"": ""Click"",
+//                    ""type"": ""Button"",
+//                    ""id"": ""d74422f0-ab3a-49ef-a6c7-789ea2f3bd2b"",
+//                    ""expectedControlType"": ""Button"",
+//                    ""processors"": """",
+//                    ""interactions"": """"
+//                }
+//            ],
+//            ""bindings"": [
+//                {
+//                    ""name"": """",
+//                    ""id"": ""2ec476de-498d-49ed-bfdf-3aa195c2c3b4"",
+//                    ""path"": ""<Mouse>/leftButton"",
+//                    ""interactions"": ""Press(behavior=1)"",
+//                    ""processors"": """",
+//                    ""groups"": """",
+//                    ""action"": ""Click"",
+//                    ""isComposite"": false,
+//                    ""isPartOfComposite"": false
+//                }
+//            ]
+//        }
+//    ],
+//    ""controlSchemes"": []
+//}");
+//        // Mouse
+//        m_Mouse = asset.FindActionMap("Mouse", throwIfNotFound: true);
+//        m_Mouse_Click = m_Mouse.FindAction("Click", throwIfNotFound: true);
+//    }
 
-    public void Dispose()
-    {
-        UnityEngine.Object.Destroy(asset);
-    }
+//    public void Dispose()
+//    {
+//        UnityEngine.Object.Destroy(asset);
+//    }
 
-    public InputBinding? bindingMask
-    {
-        get => asset.bindingMask;
-        set => asset.bindingMask = value;
-    }
+//    public InputBinding? bindingMask
+//    {
+//        get => asset.bindingMask;
+//        set => asset.bindingMask = value;
+//    }
 
-    public ReadOnlyArray<InputDevice>? devices
-    {
-        get => asset.devices;
-        set => asset.devices = value;
-    }
+//    public ReadOnlyArray<InputDevice>? devices
+//    {
+//        get => asset.devices;
+//        set => asset.devices = value;
+//    }
 
-    public ReadOnlyArray<InputControlScheme> controlSchemes => asset.controlSchemes;
+//    public ReadOnlyArray<InputControlScheme> controlSchemes => asset.controlSchemes;
 
-    public bool Contains(InputAction action)
-    {
-        return asset.Contains(action);
-    }
+//    public bool Contains(InputAction action)
+//    {
+//        return asset.Contains(action);
+//    }
 
-    public IEnumerator<InputAction> GetEnumerator()
-    {
-        return asset.GetEnumerator();
-    }
+//    public IEnumerator<InputAction> GetEnumerator()
+//    {
+//        return asset.GetEnumerator();
+//    }
 
-    IEnumerator IEnumerable.GetEnumerator()
-    {
-        return GetEnumerator();
-    }
+//    IEnumerator IEnumerable.GetEnumerator()
+//    {
+//        return GetEnumerator();
+//    }
 
-    public void Enable()
-    {
-        asset.Enable();
-    }
+//    public void Enable()
+//    {
+//        asset.Enable();
+//    }
 
-    public void Disable()
-    {
-        asset.Disable();
-    }
+//    public void Disable()
+//    {
+//        asset.Disable();
+//    }
 
-    // Mouse
-    private readonly InputActionMap m_Mouse;
-    private IMouseActions m_MouseActionsCallbackInterface;
-    private readonly InputAction m_Mouse_Click;
-    public struct MouseActions
-    {
-        private @CursoreControll m_Wrapper;
-        public MouseActions(@CursoreControll wrapper) { m_Wrapper = wrapper; }
-        public InputAction @Click => m_Wrapper.m_Mouse_Click;
-        public InputActionMap Get() { return m_Wrapper.m_Mouse; }
-        public void Enable() { Get().Enable(); }
-        public void Disable() { Get().Disable(); }
-        public bool enabled => Get().enabled;
-        public static implicit operator InputActionMap(MouseActions set) { return set.Get(); }
-        public void SetCallbacks(IMouseActions instance)
-        {
-            if (m_Wrapper.m_MouseActionsCallbackInterface != null)
-            {
-                @Click.started -= m_Wrapper.m_MouseActionsCallbackInterface.OnClick;
-                @Click.performed -= m_Wrapper.m_MouseActionsCallbackInterface.OnClick;
-                @Click.canceled -= m_Wrapper.m_MouseActionsCallbackInterface.OnClick;
-            }
-            m_Wrapper.m_MouseActionsCallbackInterface = instance;
-            if (instance != null)
-            {
-                @Click.started += instance.OnClick;
-                @Click.performed += instance.OnClick;
-                @Click.canceled += instance.OnClick;
-            }
-        }
-    }
-    public MouseActions @Mouse => new MouseActions(this);
-    public interface IMouseActions
-    {
-        void OnClick(InputAction.CallbackContext context);
-    }
-}
+//    // Mouse
+//    private readonly InputActionMap m_Mouse;
+//    private IMouseActions m_MouseActionsCallbackInterface;
+//    private readonly InputAction m_Mouse_Click;
+//    public struct MouseActions
+//    {
+//        private @CursoreControll m_Wrapper;
+//        public MouseActions(@CursoreControll wrapper) { m_Wrapper = wrapper; }
+//        public InputAction @Click => m_Wrapper.m_Mouse_Click;
+//        public InputActionMap Get() { return m_Wrapper.m_Mouse; }
+//        public void Enable() { Get().Enable(); }
+//        public void Disable() { Get().Disable(); }
+//        public bool enabled => Get().enabled;
+//        public static implicit operator InputActionMap(MouseActions set) { return set.Get(); }
+//        public void SetCallbacks(IMouseActions instance)
+//        {
+//            if (m_Wrapper.m_MouseActionsCallbackInterface != null)
+//            {
+//                @Click.started -= m_Wrapper.m_MouseActionsCallbackInterface.OnClick;
+//                @Click.performed -= m_Wrapper.m_MouseActionsCallbackInterface.OnClick;
+//                @Click.canceled -= m_Wrapper.m_MouseActionsCallbackInterface.OnClick;
+//            }
+//            m_Wrapper.m_MouseActionsCallbackInterface = instance;
+//            if (instance != null)
+//            {
+//                @Click.started += instance.OnClick;
+//                @Click.performed += instance.OnClick;
+//                @Click.canceled += instance.OnClick;
+//            }
+//        }
+//    }
+//    public MouseActions @Mouse => new MouseActions(this);
+//    public interface IMouseActions
+//    {
+//        void OnClick(InputAction.CallbackContext context);
+//    }
+//}
