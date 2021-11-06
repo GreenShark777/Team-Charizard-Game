6eabc24937f0c1dcdc35646530c80aeb2498f301
GreenShark777 <greenshark1740@gmail.com>
2021-11-06 15:33:21 +0100

    Creato script per danni delle trappole o cose che fanno danno

---
+++ Team Charizard Game/Assets/Scripts/Traps/DamagingCollider.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamagingCollider : MonoBehaviour, IGiveDamage
+{
+
+    [SerializeField]
+    private float dmg = default;
+
+
+    public float GiveDamage()
+    {
+
+        return dmg;
+
+    }
+
+}
