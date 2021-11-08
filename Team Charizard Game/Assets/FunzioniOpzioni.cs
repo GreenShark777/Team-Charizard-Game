a71c54ebb6d6a5a9166bf2232d861691bdf8d830
WorldSilver <85555472+WorldSilver@users.noreply.github.com>
2021-11-08 19:23:41 +0100

    sistemato ril proproblema del salvataggio impostazioni
    tranne audio

--- Team Charizard Game/Assets/FunzioniOpzioni.cs
+++ Team Charizard Game/Assets/FunzioniOpzioni.cs
@@ -2,24 +2,29 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-public class FunzioniOpzioni : MonoBehaviour
+public class FunzioniOpzioni : MonoBehaviour,IUpdateData
 {
-    public Dropdown risoluzioneDropdown;
+    public Dropdown graficaDropdown, risoluzioneDropdown;
     Resolution[] resolutions;
+    public Toggle toggleFullSchermo;
+
+    [SerializeField]
+    private GameManag g=default;
     private void Start()
     {
+
         resolutions = Screen.resolutions;
         risoluzioneDropdown.ClearOptions();
 
         List<string> opzioni = new List<string>();
 
         int currentResolutionIndex = 0;
-        for (int i=0; i < resolutions.Length; i++)
+        for (int i = 0; i < resolutions.Length; i++)
         {
             string option = resolutions[i].width + "x" + resolutions[i].height;
             opzioni.Add(option);
 
-            if(resolutions[i].width == Screen.currentResolution.width && resolutions[i].height == Screen.currentResolution.height)
+            if (resolutions[i].width == Screen.currentResolution.width && resolutions[i].height == Screen.currentResolution.height)
             {
                 currentResolutionIndex = i;
             }
@@ -27,7 +32,10 @@ public class FunzioniOpzioni : MonoBehaviour
         risoluzioneDropdown.AddOptions(opzioni);
         risoluzioneDropdown.value = currentResolutionIndex;
         risoluzioneDropdown.RefreshShownValue();
-
+        graficaDropdown.value = g.qualita;
+        risoluzioneDropdown.value = g.risoluzione;
+        toggleFullSchermo.isOn = g.fullSchermo;
+        gameObject.SetActive(false);
     }
 
 
@@ -47,6 +55,12 @@ public class FunzioniOpzioni : MonoBehaviour
         Screen.fullScreen = isFullSchermo;
     }
 
-
-
+    public void UpdateData()
+    {
+        // serve per salvare la qualita selezionata 
+        g.qualita = graficaDropdown.value;
+        g.risoluzione = risoluzioneDropdown.value;
+        Debug.Log("Valiu"+graficaDropdown.value);
+        g.fullSchermo = toggleFullSchermo.isOn;
+    }
 }
---
+++ Team Charizard Game/Assets/Scripts/Save System/GameManag.cs
@@ -0,0 +1,153 @@
+﻿//si occupa di tenere conto delle variabili salvate quando viene caricato il gioco
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class GameManag : MonoBehaviour
+{
+  
+    
+
+    //indica se vogliamo caricare i dati ad inizio scena o meno(PER DEBUG, COMMENTARE A GIOCO FINITO)
+    [SerializeField]
+    private bool loadData = true;
+
+    public float savedMasterVolume = 0, //indica il valore del volume generale scelto dal giocatore l'ultima volta che è stato salvato
+        savedMusicVolume = -25, //indica il valore del volume della musica scelto dal giocatore l'ultima volta che è stato salvato
+        savedSfxVolume = -15; //indica il valore del volume degli effetti sonori scelto dal giocatore l'ultima volta che è stato salvato
+
+    public int savedLanguage = 0; //indica la lingua che è stata messa l'ultima volta dal giocatore
+
+    public int qualita=0,
+               risoluzione=0;
+
+    public bool fullSchermo;
+
+    //riferimento a tutti gli script che usano l'interfaccia per l'aggiornamento dei dati nel GameManag
+    public static List<IUpdateData> dataToSave = new List<IUpdateData>();
+
+
+    private void Awake()
+    {
+        /*
+        //carica i dati nel GameManag, se non stanno venendo cancellati
+        if (!SaveSystem.isDeleting) { OnGameLoad(SaveSystem.LoadGame()); }
+        //altrimenti, comunica che è stato caricato il gioco di nuovo, quindi il cancellamento dei dati è finito
+        else { SaveSystem.isDeleting = false; OnGameLoad(SaveSystem.LoadGame()); }//SaveSystem.DataSave(this); OnGameLoad(SaveSystem.LoadGame()); }
+        */
+        //se i dati stavano venendo cancellati, indica che il cancellamento è finito in quanto si stanno per caricare i dati
+        if (SaveSystem.isDeleting) { SaveSystem.isDeleting = false; }
+        //carica i dati salvati
+        if(loadData) OnGameLoad(SaveSystem.LoadGame());
+        //dopo il caricamento dei dati controlla se gli array sono vuoti, nel qual caso li inizializza
+        InizializeEmptyArrays();
+
+    }
+
+    private void Start()
+    {
+        //viene svuotata la lista di script che devono salvare i dati
+        dataToSave.Clear();
+        //viene creato un'array recipiente con tutti gli script che devono salvare dati(anche quelli inattivi)
+        var recipient = FindObjectsOfType<MonoBehaviour>().OfType<IUpdateData>();
+        //inizializza la lista di script che devono salvare i dati, aggiungendo tutti gli elementi nella lista recipiente
+        foreach (IUpdateData elem in recipient) { dataToSave.Add(elem); }
+
+        //DEBUG PER SAPERE GLI OGGETTI NELLA LISTA DI CHI DEVE AGGIORNARE DATI PRIMA DEL SALVATAGGIO
+        //for (int i = 0; i < dataToSave.Count; i++ ) { Debug.LogError("Aggiunto: " + dataToSave[i]); }
+        //Debug.Log("Oggetti nella lista di dati da salvare: " + dataToSave.Count);
+    }
+    /// <summary>
+    /// Carica i dati salvati in SaveData
+    /// </summary>
+    /// <param name="sd"></param>
+    public void OnGameLoad(SaveData sd)
+    {
+        //se esistono dati di salvataggio
+        if (sd != null)
+        {
+            //aggiorna i dati in base ai dati salvati su SaveData
+            savedMasterVolume = sd.savedMasterVolume;
+            savedMusicVolume = sd.savedMusicVolume;
+            savedSfxVolume = sd.savedSfxVolume;
+            savedLanguage = sd.savedLanguage;
+            qualita = sd.qualita;
+            risoluzione = sd.risoluzione;
+            fullSchermo = sd.fullSchermo;
+            Debug.Log("Caricati dati salvati");
+        } //altrimenti, tutti i dati vengono messi al loro valore originale, in quanto non si è trovato un file di salvataggio
+        else { DataErased(); }
+
+    }
+    /// <summary>
+    /// Riporta tutti i dati salvati al loro valore originale
+    /// </summary>
+    public void DataErased()
+    {
+        //tutte le variabili vengono riportate al loro valore originale
+        savedMasterVolume = 0;
+        savedMusicVolume = -25;
+        savedSfxVolume = -15;
+        savedLanguage = 0;
+        qualita = 0;
+        risoluzione = 0;
+        fullSchermo = true;
+
+
+
+        //tutti gli array vengono svuotati
+        EmptyArrays();
+
+        Debug.Log("Caricati dati iniziali");
+    }
+    /// <summary>
+    /// Riporta tutti gli array al loro valore originale
+    /// </summary>
+    private void EmptyArrays()
+    {
+        //variabile di controllo che indicherà quanti cicli hanno fatto i cicli for sottostanti
+        //int nControl = 0;
+        //l'array di stanze viste dal giocatore viene svuotato(riportando tutte le stanze a non viste)
+        //for (int i = 0; i < seenRooms.Length; i++) { seenRooms = new bool[N_ROOMS]; /*nControl++;*/ }
+       
+        //Debug.Log("Cicli fatti per i frammenti ottenuti: " + nControl); nControl = 0;
+    }
+    /// <summary>
+    /// Controlla, per ogni array, se è nullo, nel qual caso inizializza l'array nel modo necessario
+    /// </summary>
+    private void InizializeEmptyArrays()
+    {
+        //se l'array di stanze viste dal giocatore è nullo, viene inizializzato
+       // if (seenRooms == null) { seenRooms = new bool[N_ROOMS]; /*Debug.Log("Lista");*/ }
+       
+    }
+    /// <summary>
+    /// Aggiorna i dati da salvare nel GameManag prima di salvare i dati
+    /// </summary>
+    private void UpdateDataBeforeSave()
+    {
+        //variabile di controllo, per vedere quante volte viene svolto il ciclo foreach
+       int n = 0;
+        //viene richiamata la funzione dell'interfaccia per aggiornare i dati di ogni elemento nella lista
+        foreach (IUpdateData elem in dataToSave) { elem.UpdateData(); n++; }
+        Debug.Log("Aggiornati dati nel GameManag. Il numero di elementi aggiornati sono: " + n);
+    }
+
+    public void SaveDataAfterUpdate()
+    {
+        //aggiorna i dati se la scena non è un livello o, se lo è, se il livello è stato completato
+        UpdateDataBeforeSave();
+        //salva i dati ogni volta che si va da una scena all'altra, se i dati non stanno venendo cancellati
+        if (!SaveSystem.isDeleting) { SaveSystem.DataSave(this); Debug.Log("Dati aggiornati e salvati"); }
+        else Debug.LogError("Dati non aggiornati, perchè stanno venendo cancellati");
+
+    }
+
+    private void OnDestroy()
+    {
+        //salva i dati di gioco dopo aver ottenuto gli aggiornamenti dalla lista di script che devono aggiornare i dati
+        /*if(gameObject.scene.buildIndex == 0)*/ SaveDataAfterUpdate();
+
+    }
+
+}
