f74c5e23d4285217fd2121111c5066a46244db54
WorldSilver <85555472+WorldSilver@users.noreply.github.com>
2021-11-11 12:57:14 +0100

    BOMB E CPROVA FALLIMENTO

---
+++ Team Charizard Game/Assets/BombaCHEbomba.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BombaCHEbomba : MonoBehaviour
+{
+    public GameObject bobmba;
+    public Rigidbody rig;
+    public float speed;
+    public Transform spawn;
+    bool attivati;
+    private void Start()
+    {
+        rig = GetComponent<Rigidbody>();
+
+
+    }
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.RightShift))
+        {
+            attivati = true;
+        }
+       
+    }
+    private void FixedUpdate()
+    {
+        if (attivati)
+        {
+            lanciaBomb();
+        }
+    }
+    private  void lanciaBomb()
+    {
+        bobmba.transform.rotation = Quaternion.LookRotation(spawn.forward);
+        Rigidbody rig = bobmba.GetComponent<Rigidbody>();
+        rig.AddForce(spawn.forward * speed,ForceMode.Impulse);
+        attivati = false;
+    }
+}
