3c94659cebabd28ba68b70e571e2892252c1e132
GreenShark777 <greenshark1740@gmail.com>
2021-11-14 01:04:28 +0100

    Creati script per UI classifica e per il podio

---
+++ Team Charizard Game/Assets/Scripts/Circuits/PodioPlacement.cs
@@ -0,0 +1,22 @@
+﻿//Si occupa del posizionamento dei veicoli nel podio
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PodioPlacement : MonoBehaviour
+{
+
+    private float heightOffset = 5;
+
+    [SerializeField]
+    private Transform playerKart = default;
+
+
+    public void PlaceVehicles()
+    {
+
+
+        Debug.Log("Posizionati veicoli nel podio");
+    }
+
+}
