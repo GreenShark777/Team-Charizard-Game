daf76b60bfe9c66af20690e8f3acdd2a103c81db
iTzMeMario <fedemacri27@gmail.com>
2021-11-15 12:11:20 +0100

    Iniziato Tutorial

---
+++ Team Charizard Game/Assets/Scripts/Tutorial/tutorialTrigger.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class tutorialTrigger : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject image;
+    [SerializeField]
+    private float time;
+    [SerializeField]
+    private bool isImageVisible = false;
+    void Start()
+    {
+        
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+
+
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            //isImageVisible = true;
+
+            StartCoroutine(showImage());
+        }
+
+
+    }
+
+    IEnumerator showImage()
+    {
+       
+            image.SetActive(true);
+            Time.timeScale = 0.2f;
+    
+            yield return new WaitForSeconds(time);
+            image.SetActive(false);
+            Time.timeScale = 1;
+
+
+        
+
+    }
+
+}
